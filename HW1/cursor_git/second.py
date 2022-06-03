fdc3457c2c0a1b676bd5491c7f876be66ad46950
Oleksandr Ilin <vardentheknight@gmail.com>
2022-06-03 13:21:34 +0300

    Make changes

--- HW1/cursor_git/second.py
+++ HW1/cursor_git/second.py
@@ -1,16 +1,2 @@
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-pring('Hello')
-=======
 print('Hello')
->>>>>>> Stashed changes
-=======
-print('Hello')
->>>>>>> Stashed changes
-=======
-print('Hello')
->>>>>>> Stashed changes
->>>>>>> second
+print('Cursor')
